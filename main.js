e59930ca0330eb681fddf7dddc5474f83f24eeac
VRosa-jpg <vixkirosa@gmail.com>
2024-11-04 12:30:44 -0500

    Update: Dropdown

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 // Dropdown - Variable
-const dropdown = document.querySelector(".dropdown")
-const dropdownItems = document.querySelectorAll(".dropdown-item")
+// const dropdown = document.querySelector(".dropDown")
+// const dropdownItems = document.querySelectorAll(".dropdown-item")
 
-// Eventlistener
-dropdown.addEventListener("mouseover", (event) => {
-    dropdownItems.forEach((item) => {
-        console.log(item);
-    })
-})
+
+// // Eventlistener
+// dropdown.addEventListener("mouseover", (event) => {
+//     dropdownItems.forEach((item) => {
+//         // console.log(item);
+        
+//     })
+// })
+
+document.getElementById('dropDown').addEventListener('mouseover', () => {
+    document.getElementById('dropdownContent').style.display = 'block';
+});
+
+document.getElementById('dropDown').addEventListener('mouseout', function() {
+    document.getElementById('dropdownContent').style.display = 'none';
+});
